19d3e7ad6565c64c8e27cad7dc7e7c36055fb8ba
vinhltaz <vinhltaz@gmail.com>
2019-12-17 10:00:14 +0700

    config - connect to oracledb

--- db.js
+++ db.js
@@ -1,38 +1,35 @@
-const Orcl = require('oracledb');
+const Orcl = require("oracledb");
 Orcl.outFormat = Orcl.OUT_FORMAT_OBJECT;
 
-module.exports =  class DB {
-    constructor() {
-        this.hostname = 'SERVER';
-        this.servicename = 'ORCLPDB.LOCALDOMAIN';
-        this.username = 'PASSPORT';
-        this.password = 'PASSPORT';
-    }
-    async run() {
-        let connection;
+module.exports = class DB {
+  constructor() {
+    this.hostname = "192.168.188.128";
+    this.servicename = "orclpdb.localdomain";
+    this.username = "HR";
+    this.password = "hr";
+  }
+  async run() {
+    let connection;
+    try {
+      connection = await Orcl.getConnection({
+        user: this.username,
+        password: this.password,
+        connectString: `${this.hostname}:1521/${this.servicename}`
+      });
+
+      const result = await connection.execute("SELECT * FROM REGIONS");
+
+      console.log(result.rows);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      if (connection) {
         try {
-            connection = await Orcl.getConnection({
-                user: this.username,
-                password: this.password,
-                connectString: `${this.hostname}/${this.servicename}`
-            });
-
-            const result = await connection.execute('SELECT * FROM RELIGION');
-
-            console.log(result.rows);
+          await connection.close();
+        } catch (err) {
+          console.log(err);
         }
-        catch (err) {
-            console.log(err);
-        }
-        finally {
-            if (connection) {
-                try {
-                    await connection.close();
-                }
-                catch (err) {
-                    console.log(err);
-                }
-            }
-        }
+      }
     }
-}
+  }
+};
