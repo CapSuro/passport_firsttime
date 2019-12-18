9e1d818a6b26a09c03d1db9004fd210b537e2a59
hungtp <hungtp.personal@gmail.com>
2019-12-18 10:59:59 +0700

    repair for backend page

---
+++ src/BackEndSite/BackEndEntry/BackEndEntry.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import React from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HomePageBackEnd } from './../HomePage/HomePageBackEnd';
+import { Saving } from './../Saving/Saving';
+import { Approval } from './../Approval/Apporval';
+const { Header, Content, Footer, Sider } = Layout;
+
+export const Entry = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (<Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
+            <div className="logo">QikPassport</div>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="requestform">
+                    <Link to="/"><Icon type="file" />
+                        <span>Request Form</span></Link>
+                </Menu.Item>
+                <Menu.Item key="approval">
+                    <Link to="/approval"><Icon type="form" />
+                        <span>Approval</span></Link>
+                </Menu.Item>
+                <Menu.Item key="saving">
+                    <Link to="saving"><Icon type="monitor" />
+                        <span>Saving</span></Link>
+                </Menu.Item>
+            </Menu>
+        </Sider>
+        <Layout>
+            <Header style={{ background: '#fff', padding: 0 }} />
+            <Content style={{ margin: '0 16px' }}>
+                {props.location['pathname'] === "/saving" ? <Saving /> :
+                    props.location['pathname'] === "/approval" ? <Approval /> : <HomePageBackEnd />}
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>QIkPassport ©2018 all right reserved</Footer>
+        </Layout>
+    </Layout>)
+}
---
+++ src/BackEndSite/Saving/Saving.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Saving = (props) => {
+    return <h2>This is saving page</h2>
+}
