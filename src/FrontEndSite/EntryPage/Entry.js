4bdc6a4d7f31d882b4294cdb8be45503ce5ef00a
hungtp <hungtp.personal@gmail.com>
2019-12-18 10:45:16 +0700

    update menu

--- src/FrontEndSite/EntryPage/Entry.js
+++ src/FrontEndSite/EntryPage/Entry.js
@@ -27,19 +27,6 @@ export const Entry = (props) => {
                     <Icon type="monitor" />
                     <span><Link to="monitor">Monitor</Link></span>
                 </Menu.Item>
-                <SubMenu
-                    key="sub1"
-                    title={
-                        <span>
-                            <Icon type="form" />
-                            <span>User</span>
-                        </span>
-                    }
-                >
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
             </Menu>
         </Sider>
         <Layout>
