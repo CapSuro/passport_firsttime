3ba09acac2c4046e1465bcca26746871fbe667f1
hungtp <hungtp.personal@gmail.com>
2019-12-18 12:57:20 +0700

    update monitor page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './FrontEndSite/HomePage/HomePage.css'
 import {BrowserRouter as Router, Route,Switch, Redirect} from 'react-router-dom';
-import { BackendEntry } from './BackEndSite/BackEndEntry/BackEndEntry';
-
+//import { BackendEntry } from './BackEndSite/BackEndEntry/BackEndEntry';
+import { Entry } from './FrontEndSite/EntryPage/Entry';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" component={BackendEntry}></Route>
+          <Route path="/" component={Entry}></Route>
           <Redirect to="/" />
         </Switch>
       </Router>
--- src/FrontEndSite/MonitorPage/MonitorPage.js
+++ src/FrontEndSite/MonitorPage/MonitorPage.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import { Icon, Input, Form, Button } from 'antd';
-import { Table } from 'antd';
+import { Table, Tag, Tooltip } from 'antd';
+
+const { Column } = Table;
+
+const getColor = (state) => {
+    if (state === 'CN') return 'blue';
+    else if (state === 'CXT') return 'red';
+    else if (state === 'CD') return 'green';
+    else if (state === 'KD') return 'black';
+    else if (state === 'KXT') return 'black';
+    else return 'orange';
+}
+
+const getTooltip = (state) => {
+    if (state === 'CN') return 'waiting for receiving';
+    else if (state === 'CXT') return 'waiting for checking';
+    else if (state === 'CD') return 'waiting for approval';
+    else if (state === 'KD') return 'approval denied';
+    else if (state === 'KXT') return 'not validated';
+    else return 'approved';
+}
 
 const SearchForm = Form.create({ name: 'search_form' })((props) => {
     const { getFieldDecorator } = props.form;
@@ -22,46 +42,60 @@ const SearchForm = Form.create({ name: 'search_form' })((props) => {
 
 
 const TablePassport = (props) => {
-    const columns = [{
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
-    }, {
-        title: 'Fullname',
-        dataIndex: 'fullname',
-        key: 'fullname'
-    }, {
-        title: 'Regist Date',
-        dataIndex: 'registdate',
-        key: 'registdate'
-    }, {
-        title: 'State',
-        dataIndex: 'state',
-        key: 'state'
-    }];
-
     const tempalteData = [
         {
             id: '1',
             fullname: 'Hung Dep Trai',
             registdate: '12-8-2019',
-            state: 'DN'
+            state: 'CN'
         },
         {
             id: '2',
             fullname: 'Hung Dep Trai',
             registdate: '12-8-2019',
-            state: 'DN'
+            state: 'CXT'
         },
         {
             id: '3',
             fullname: 'Hung Dep Trai',
             registdate: '12-8-2019',
-            state: 'DN'
+            state: 'CD'
+        },
+        {
+            id: '4',
+            fullname: 'Hung Dep Trai',
+            registdate: '12-8-2019',
+            state: 'KXT'
+        },
+        {
+            id: '5',
+            fullname: 'Hung Dep Trai',
+            registdate: '12-8-2019',
+            state: 'DD'
+        },
+        {
+            id: '6',
+            fullname: 'Hung Dep Trai',
+            registdate: '12-8-2019',
+            state: 'KD'
         },
     ]
 
-    return <Table columns={columns} dataSource={tempalteData} />
+    return <Table dataSource={tempalteData}>
+        <Column title="ID" dataIndex="id" key="id" />
+        <Column title="Fullname" dataIndex="fullname" key="fullname" />
+        <Column title="Regist Date" dataIndex="registdate" key="registdate" />
+        <Column title="State" dataIndex="state" key="state"
+            render={
+                state => (
+                    <span>
+                        <Tooltip title={getTooltip(state)}>
+                            <Tag color={getColor(state)}>{state}</Tag>
+                        </Tooltip>
+                    </span>
+                )
+            } />
+    </Table>
 }
 
 export const MonitorPage = (props) => {
